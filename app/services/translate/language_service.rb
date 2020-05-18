0d888bc212f2a35bdb0f059fb2607155268ec8cb
José Luiz Junior <junior.jr.jl@gmail.com>
2020-05-18 10:10:51 -0300

    verificando url de post

--- app/services/translate/language_service.rb
+++ app/services/translate/language_service.rb
@@ -10,8 +10,9 @@ module TranslateModule
         end
 
         def call
-            response = JSON.parse(RestClient.post @url, {})
+            response = JSON.parse(RestClient.post @url, {}.to_json, {Accept: '*/*', Content-Type: 'application/x-www-form-urlencoded'}.to_json)
             raise ApiError::ApiComunicationError.new('Error when try comunicate with API.') if response.has_value?(:code)
+            p response
             langs = response[:langs].map {| value, key | "#{value} = #{key}"}.join(',')
             languages = translate_names(langs)
             message = "Esses são os idiomas que eu conheço: \n\n"
