1f7f61b5d47850a0bd12ecd2aa0887e6741c534a
José Luiz Junior <junior.jr.jl@gmail.com>
2020-05-18 09:03:05 -0300

    verificando o corpo da requisição

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
     post '/webhook' do
         request.body.rewind
         result = JSON.parse(request.body.read)["queryResult"]
+        p result
         if result["contexts"].present?
             response = InterpretService.call(result["action"], result["context"][0]["parameters"])
         else
