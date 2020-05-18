59eb738599839a3433da0f09e4ccebf90b6da929
José Luiz Junior <junior.jr.jl@gmail.com>
2020-05-18 11:12:34 -0300

    verificando url de post

--- app/services/translate/translate_service.rb
+++ app/services/translate/translate_service.rb
@@ -15,8 +15,7 @@ module TranslateModule
 
         def call
             raise ApiError::ApiTextMaxSizeError.new("A text #{@text} exceed a 10.000 characters.") if @text.length > 10000
-            response = JSON.parse(RestClient.post @url, {})
-            p response
+            response = JSON.parse(RestClient.post @url, {}, {'Accept' => '*/*', 'Content-Type' => 'application/x-www-form-urlencoded'})
             status_code = response['code']
             case status_code
             when ApiCode::TRANSLATED
